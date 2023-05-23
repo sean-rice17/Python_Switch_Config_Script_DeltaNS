098e3c0c6548a14fae7039f40105d7bbbaaba2ca
Sean Rice <seanrice872@gmail.com>
2023-05-23 07:11:02 -0700

    OUI pattern matching

--- Python_XOS_SwitchOUI_Script_REV0_SDR.py
+++ Python_XOS_SwitchOUI_Script_REV0_SDR.py
@@ -1,3 +1,21 @@
 # version 0
 
-substring1 = "((\w+:){2}(\w+))"
+import re
+
+# build regex patterns to use later
+# OUI pattern
+pattern1 = "((\w+:){3})"
+
+# input stream from 'sho fdb' command
+show_fdb = open("show_fdb.log", 'r')
+
+fdb_file = open("fdb_file", 'w')
+
+# find OUIs
+for line in show_fdb:
+    match = re.search(pattern1, line)
+    if match != None:
+        print(line)
+        fdb_file.write(line)
+
+fdb_file.close()
