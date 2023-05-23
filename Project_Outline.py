84e900156ebbfbf4fefcd57d12f3b4ed199f14b6
Sean Rice <seanrice872@gmail.com>
2023-05-23 09:21:46 -0700

    updated project backlog

--- Project_Outline.py
+++ Project_Outline.py
@@ -5,7 +5,7 @@ DONE - 2. prompt user for OUI and filter results by OUI
 DONE -  display filtered results to user from file
 DONE - 3. prompt user for uplink ports (up to 10)
 DONE -  store user input in array
-  display resultx filtered by these ports to user from file
+DONE -  display resultx filtered by these ports to user from file
 4. allow user to configure remaining ports
   configure ['Vlan name'] add ports [non-uplink ports] [tag status]
   prompt user for vlan name, port(s) and tag then run command
