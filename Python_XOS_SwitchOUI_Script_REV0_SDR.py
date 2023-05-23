bca71c4f203717f30fa96221617ae6336f27cfc8
Joshua Blackburn <113396594+joshuablackburn2224@users.noreply.github.com>
2023-05-23 11:04:23 -0400

    Created 2D array of devices, began OUI filter logic

--- Python_XOS_SwitchOUI_Script_REV0_SDR.py
+++ Python_XOS_SwitchOUI_Script_REV0_SDR.py
@@ -8,10 +8,10 @@ pattern1 = "((\w+:){3})"
 
 # input stream from 'sho fdb' command
 show_fdb = open("show_fdb.log", 'r')
-
+#create file to write device data to from showfdb
 fdb_file = open("fdb_file.txt", 'w')
 
-# find OUIs
+# use Regex to find Device Data based on OUI pattern match, then write data to file
 for line in show_fdb:
     match = re.search(pattern1, line)
     if match != None:
@@ -19,3 +19,36 @@ for line in show_fdb:
         fdb_file.write(line)
 
 fdb_file.close()
+
+# reopen data file for reading, create 2D array where each index corresponds
+# to one device discovered using show fdb
+deviceDataFile = open('fdb_file.txt', 'r')
+deviceListInitial = deviceDataFile.readlines()
+i = 0
+j = 0
+k = 0
+deviceList2Darr = [[0]*2]*2
+while i < len(deviceListInitial):
+    currentDevice = deviceListInitial[j].split()
+    deviceList2Darr[j] = currentDevice
+    j += 1
+while i < len(deviceList2Darr):
+    while k < len(deviceList2Darr[i]):
+        print(deviceList2Darr[i][k])
+        k += 1
+    i += 1
+    k = 0
+
+#prompt user for OUI and delete all items from list that do not contain that OUI
+
+OUI = "d8:c7:c8"
+#str(input("Enter an OUI: \n"))
+for i in range(len(deviceList2Darr)):
+    if OUI not in deviceList2Darr[i][0]:
+        deviceList2Darr.pop(i)
+
+for i in range(len(deviceList2Darr)):
+    print(deviceList2Darr[i])
+
+
+
