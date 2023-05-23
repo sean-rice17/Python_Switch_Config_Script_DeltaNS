41f3764a871b2b160b16515d91eadeaef0f300d6
Joshua Blackburn <113396594+joshuablackburn2224@users.noreply.github.com>
2023-05-23 09:38:17 -0400

    Wrote Outline File

---
+++ Project_Outline.py
@@ -0,0 +1,12 @@
+""" Outline for project
+1. sho fdb and dispaly results to user
+  write results to a file
+2. prompt user for OUI and filter results by OUI
+  display filtered results to user from file
+3. prompt user for uplink ports (up to 10)
+  -store user input in array
+  display resultx filtered by these ports to user from file
+4. allow user to configure remaining ports
+  configure ['Vlan name'] add ports [non-uplink ports] [tag status]
+  prompt user for vlan name, port(s) and tag then run command
+"""
