f2c02211de7d0eba7babb9df4270f8e3402c71ae
Joshua Blackburn <113396594+joshuablackburn2224@users.noreply.github.com>
2023-05-24 11:10:35 -0400

    Added logic for sending command to exos and wrapped function in loop

--- Python_XOS_SwitchOUI_Script_REV0_SDR.py
+++ Python_XOS_SwitchOUI_Script_REV0_SDR.py
@@ -12,131 +12,138 @@ def exosCmd(cmd):
         print result
         return result
 #
+def configSwitchVlans():
+    # build regex patterns to use later
+    # OUI pattern
 
-# build regex patterns to use later
-# OUI pattern
+    pattern1 = "((\w+:){3})"
 
-pattern1 = "((\w+:){3})"
+    # input stream from 'sho fdb' command
+    show_fdb = open("show_fdb.log", 'r')
+    # show_fdb = exosCmd('sho fdb')
+    #create file to write device data to from showfdb
+    fdb_file = open("fdb_file.txt", 'w')
 
-# input stream from 'sho fdb' command
-show_fdb = open("show_fdb.log", 'r')
-# show_fdb = exosCmd('sho fdb')
-#create file to write device data to from showfdb
-fdb_file = open("fdb_file.txt", 'w')
+    # use Regex to find Device Data based on OUI pattern match, then write data to file
+    for line in show_fdb:
+        match = re.search(pattern1, line)
+        if match != None:
+            # print line
+            fdb_file.write(line)
 
-# use Regex to find Device Data based on OUI pattern match, then write data to file
-for line in show_fdb:
-    match = re.search(pattern1, line)
-    if match != None:
-        # print line
-        fdb_file.write(line)
+    fdb_file.close()
 
-fdb_file.close()
+    # reopen data file for reading, create 2D array where each index corresponds
+    # to one device discovered using show fdb
+    deviceDataFile = open('fdb_file.txt', 'r')
+    deviceListInitial = deviceDataFile.readlines()
+    i = 0
+    j = 0
+    k = 0
+    deviceList2Darr = []
+    while i < len(deviceListInitial):
+        currentDevice = deviceListInitial[i].split()
+        deviceList2Darr.append(currentDevice)
+        i += 1
+        j += 1
+    j = 0
 
-# reopen data file for reading, create 2D array where each index corresponds
-# to one device discovered using show fdb
-deviceDataFile = open('fdb_file.txt', 'r')
-deviceListInitial = deviceDataFile.readlines()
-i = 0
-j = 0
-k = 0
-deviceList2Darr = []
-while i < len(deviceListInitial):
-    currentDevice = deviceListInitial[i].split()
-    deviceList2Darr.append(currentDevice)
-    i += 1
-    j += 1
-j = 0
+    print ""
 
-print ""
+    # print contents of 2d array
+    while j < len(deviceList2Darr):
+        deviceBufferString = "--------------------"
+        print deviceBufferString
+        print ""
+        while k < len(deviceList2Darr[j]):
+            print deviceList2Darr[j][k]
+            k += 1
+        j += 1
+        k = 0
 
-# print contents of 2d array
-while j < len(deviceList2Darr):
-    deviceBufferString = "--------------------"
     print deviceBufferString
     print ""
-    while k < len(deviceList2Darr[j]):
-        print deviceList2Darr[j][k]
-        k += 1
-    j += 1
-    k = 0
 
-print deviceBufferString
-print ""
+    #prompt user for OUI
 
-#prompt user for OUI
+    OUI = str(raw_input("Enter an OUI: \n"))
+    # OUI = "00:10:49"
 
-OUI = str(raw_input("Enter an OUI: \n"))
-# OUI = "00:10:49"
+    # add entries with matching OUIs to new array
+    deviceList2darr_filtered = []
+    for i in range(len(deviceList2Darr)):
+        if OUI in deviceList2Darr[i][0]:
+            deviceList2darr_filtered.append(deviceList2Darr[i])
+        i += 1
+    # display the new array
+    for item in deviceList2darr_filtered:
+        print item
 
-# add entries with matching OUIs to new array
-deviceList2darr_filtered = []
-for i in range(len(deviceList2Darr)):
-    if OUI in deviceList2Darr[i][0]:
-        deviceList2darr_filtered.append(deviceList2Darr[i])
-    i += 1
-# display the new array
-for item in deviceList2darr_filtered:
-    print item
+    #logic for creating list of undesirable ports
+    userInput = ""
+    # uplinkPorts = ["1:40", "2:32"]
 
-#logic for creating list of undesirable ports
-userInput = ""
-# uplinkPorts = ["1:40", "2:32"]
-
-uplinkPorts = []
-print("Enter the uplink ports which will be excluded from the configuration process. Enter q to finish entering ports.")
-while userInput != 'q':
-    userInput = str(raw_input("\n"))
-    uplinkPorts.append(userInput)
-uplinkPorts.pop()
-print ""
+    uplinkPorts = []
+    print("Enter the uplink ports which will be excluded from the configuration process. Enter q to finish entering ports.")
+    while userInput != 'q':
+        userInput = str(raw_input("\n"))
+        uplinkPorts.append(userInput)
+    uplinkPorts.pop()
+    print ""
 
 
-#logic for removing uplink ports from list
+    #logic for removing uplink ports from list
 
-# FIX: filtering devices that have uplink ports does not work if the list "uplinkPorts" has more than 1 element
 
-deviceList2darr_final = []
-found = False
-for i in range(len(deviceList2darr_filtered)):
-    currentPort = deviceList2darr_filtered[i][-1]
-    for j in range(len(uplinkPorts)):
-        if currentPort == uplinkPorts[j]:
-            found = True
+    deviceList2darr_final = []
+    found = False
+    for i in range(len(deviceList2darr_filtered)):
+        currentPort = deviceList2darr_filtered[i][-1]
+        for j in range(len(uplinkPorts)):
+            if currentPort == uplinkPorts[j]:
+                found = True
+                
+        if found == False:
+            deviceList2darr_final.append(deviceList2darr_filtered[i])
+        # only add items to the list if they do not match any uplink ports
+        else:
+            found = False
             
-    if found == False:
-        deviceList2darr_final.append(deviceList2darr_filtered[i])
-    # only add items to the list if they do not match any uplink ports
-    else:
-        found = False
+
+    # print the completely filtered device list
+    # print "Final list size is", len(deviceList2darr_final)
+    for i in range(len(deviceList2darr_final)):
         
-
-# print the completely filtered device list
-# print "Final list size is", len(deviceList2darr_final)
-for i in range(len(deviceList2darr_final)):
-    
-    print deviceList2darr_final[i]
+        print deviceList2darr_final[i]
 
 
 
-#logic for sending configuration commands to switch
-
-flag = True
-while flag == True:
+    #logic for sending configuration commands to switch
     print("\nEnter the name of the vlan you want to assign to these devices.\n")
     vlanName = raw_input("Vlan Name: ")
     # TODO: loop for each item in the list
     
-    #
+    tagStatus = raw_input("tagged/untagged: ")
 
-    # TODO: do all untagged or tagged items, depending on user input
+    portListNoDoubles = []
+    portListFinal = []
+    for i in range (len (deviceList2darr_final)):
+        portListNoDoubles.append(deviceList2darr_final[i][-1])
     
-    #
+    for i in range (len (portListNoDoubles)):
+        if portListNoDoubles[i] not in portListFinal:
+            portListFinal.append(portListNoDoubles[i])
 
-    configStr = "configure " + vlanName + " add ports " + portNum + " " + tagStatus
-    # vlanPortList = exosCmd(configStr)
-    print configStr
-    print("Would you like to configure again?")
+    for i in range(len (portListFinal)):
+        portNum = portListFinal[i]
+        configStr = "configure " + vlanName + " add ports " + portNum + " " + tagStatus
+        # vlanPortList = exosCmd(configStr)
+        print configStr
+
+
+#driver loop (main)
+moreConfigs = 'y'
+while (moreConfigs != 'n'):
+    configSwitchVlans()
+    print("Would you like to configure another vlan?")
     moreConfigs = raw_input("(y/n)")
-    if moreConfigs == 'n':
-        flag = False
